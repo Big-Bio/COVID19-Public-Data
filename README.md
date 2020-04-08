a60431dbf202af36d1f44ed2af657e1443f2916b
Brandon <bjew@s-164-67-234-227.resnet.ucla.edu>
2020-04-08 12:38:39 -0700

    changed format for team effort to gather data

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
-# covidweb scraper
+# COVID-19 Public Data Curation
 
-These scripts will grab the most up-to-date COVID-19 case information from [LA County Public Health](http://publichealth.lacounty.gov/media/Coronavirus/locations.htm)
-and from [Johns Hopkins CSSE](http://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports).
+This repository stores public time series data for any COVID-19 related features, such as case counts, death counts, hospital resource usage, etc. 
 
-Data is dumped into JS files in the `archive` directory with suffix `MM-DD-YYYY.js` and into the `latest` directory with a constant suffix.
+These data should have consistent naming conventions for samples (such as location names) and structure. Specifically, data should be stored in the following manner:
 
-# Requirements
+The feature name will have a subdirectory in the `processed_data` parent directory, such as `cases` or `deaths`. Within this subdirectory, the file should be named `{sample_name}_{feature_name}.csv`. This file should be accompanied by a file named `{sample_name}_{feature_name}.txt` that describes the data in more detail, including its source. `sample_name` can be something like "los-angeles-county" or "ucla-reagan-hospital".  The data file should be comma separated with quotations. Row names should correspond to individual samples (such as Westwood in "los-angeles-county") and column names should be the date in `YYYY-MM-DD` format. Each entry corresponds to the value of the given feature on the given date. Missing values will be set to the "NA" string. 
 
-R scripts require `stringr`, `plyr`, `RCurl` packages.
+Scripts for generating these files should ideally be shared as well. These can be stored in the `scripts` parent directory. Since scripts may generate multiple processed data files, the best way to reference these scripts is to include their name in the `{sample_name}_{feature_name}.txt` file that accompanies the processed data.
 
-JS script requires `cheerio` and `request` packages
-
-# Usage
-
-From the scripts directory (i.e. make sure `scripts` is your working directory)
-
-`Rscript csse_covid_19_daily_reports.R`
-
-`Rscript la_county_daily_reports.R`
+If you have geographic data (latitude or longitude) for the samples in your file, please add it to the `geo_data` directory as `{sample_name}_coords.csv`. These should have the place name as rows and a `lat` and `lon` column.  
---
+++ scripts/pull_covid_time_series.R
@@ -0,0 +1,105 @@
+library(stringr)
+
+#' Pulls time series data from CSSE for global data
+#' @return data.frame of time series data
+read_csse_global_time_series_data <- function(csv.url){
+  tol <- 1e-2
+  raw.time.series <- read.csv(csv.url, check.names = FALSE, stringsAsFactors = FALSE)
+  raw.time.series <- raw.time.series[abs(raw.time.series$Lat) > tol & abs(raw.time.series$Long) > tol,]
+  place <- stringr::str_replace(paste(raw.time.series$`Province/State`, raw.time.series$`Country/Region`, sep=', '), "^, ", "")
+  global.time.series <- raw.time.series[,5:ncol(raw.time.series)] # assuming first 4 columns are province, country, lat, and lon
+  row.names(global.time.series) <- place
+  return(global.time.series)
+}
+
+#' Pulls time series data from CSSE for US data
+#' @return A list of state.level and county.level data.frames
+read_csse_us_time_series_data <- function(csv.url){
+  raw.time.series <- read.csv(csv.url, stringsAsFactors = FALSE, check.names = FALSE)
+  tol <- 1e-2
+  states.to.remove <- c("Grand Princess", "Diamond Princess")
+  raw.time.series <- raw.time.series[!(raw.time.series$Province_State %in% states.to.remove),]
+  # State level
+  state.level.time.series <- aggregate(raw.time.series[,12:ncol(raw.time.series)],by=raw.time.series[,"Province_State",drop=F], FUN = sum )
+  rownames(state.level.time.series) <- state.level.time.series[,"Province_State"]
+  state.level.time.series <- state.level.time.series[,2:ncol(state.level.time.series)]
+  # County level, remove places with no lat lon
+  raw.time.series <- raw.time.series[abs(raw.time.series$Lat) > tol & abs(raw.time.series$Long_) > tol,]
+  county.level.time.series <- raw.time.series[,12:ncol(raw.time.series)]
+  rownames(county.level.time.series) <- raw.time.series[,"Combined_Key"]
+  return(list(state.level=state.level.time.series, county.level=county.level.time.series))
+}
+
+#' Pulls time series data from LA Times for SoCal data
+read_latimes_agency_data <- function(csv.url){
+  socal.level.data <- read.csv(csv.url, check.names=FALSE, stringsAsFactors = FALSE)
+  # remove complete duplicate rows
+  socal.level.data <- socal.level.data[!duplicated(socal.level.data[,c("date", "county", "fips", "place")]),]
+  losangeles.data <- socal.level.data[socal.level.data$county == "Los Angeles",]
+  unincorporated.pattern <- "^Unincorporated - "
+  unincorporated.cities <- grep(unincorporated.pattern, losangeles.data$place, value = TRUE)
+  losangeles.pattern <- "^Los Angeles - "
+  losangeles.cities <- grep(losangeles.pattern, losangeles.data$place, value = TRUE)
+  cityof.pattern <- "^City of "
+  cityof.cities <- grep(cityof.pattern, losangeles.data$place, value=TRUE)
+  place.to.remove <- c("Under Investigation", "- Under Investigation", "Unknown", "Smaller Los Angeles neighborhoods",
+                       "Unincorporated Florence-Firestone", "Unincorporated - Florence-Firestone", "Los Angeles - Florence-Firestone")
+  place.to.remove <- c(place.to.remove, losangeles.cities, unincorporated.cities, cityof.cities)
+  socal.level.data <- socal.level.data[!(socal.level.data$place %in% place.to.remove),]
+  #unincorporated.la.cities <- stringr::str_replace(grep(unincorporated.pattern, losangeles.data$place, value = TRUE), unincorporated.pattern, "")
+  #unincorporated.lat <- losangeles.data[grep(unincorporated.pattern, losangeles.data$place),]$y
+  #unincorporated.lon <- losangeles.data[grep(unincorporated.pattern, losangeles.data$place),]$x
+  
+  #losangeles.la.cities <- stringr::str_replace(grep(losangeles.pattern, losangeles.data$place, value=TRUE), losangeles.pattern, "")
+  #losangeles.lat <- losangeles.data[grep(losangeles.pattern, losangeles.data$place),]$y
+  #losangeles.lon <- losangeles.data[grep(losangeles.pattern, losangeles.data$place),]$x
+  places <- paste(socal.level.data$place, socal.level.data$county, sep=', ')
+  times <- as.numeric(as.POSIXct(socal.level.data$date))
+  dates <- stringr::str_replace(format(as.POSIXct(times, origin="1970-01-01"), "%m/%d/%y"), "^0", "")
+  unique.dates <- unique(stringr::str_replace(format(as.POSIXct(sort(times), origin="1970-01-01"), "%m/%d/%y"), "^0", ""))
+  unique.places <- unique(places)
+  socal.time.series.data <- data.frame(row.names=unique.places)
+  for (datestr in unique.dates){
+    row.indices <- dates == datestr
+    sub.df <- socal.level.data[row.indices,]
+    rownames(sub.df) <- places[row.indices]
+    new.col <- sub.df[unique.places,"confirmed_cases"]
+    socal.time.series.data[[datestr]] <- new.col
+  }
+  return(socal.time.series.data)
+}
+
+main <- function(){
+  out.dir <- "../processed_data/"
+  
+  global.cases.time.series.csv <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+  global.death.time.series.csv <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+  us.cases.time.series.csv <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+  us.death.time.series.csv <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+  socal.time.series.csv <- "https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-place-totals.csv" # no deaths, just cases
+  
+  us.cases <- read_csse_us_time_series_data(us.cases.time.series.csv)
+  us.deaths <- read_csse_us_time_series_data(us.death.time.series.csv)
+  
+  socal.cases <- read_latimes_agency_data(socal.time.series.csv)
+  us.state.cases <- us.cases$state.level
+  us.county.cases <- us.cases$county.level
+  global.cases <- read_csse_global_time_series_data(global.cases.time.series.csv)
+  
+  us.state.deaths <- us.deaths$state.level
+  us.county.deaths <- us.deaths$county.level
+  global.deaths <- read_csse_global_time_series_data(global.death.time.series.csv)
+  
+  write.csv(socal.cases, sprintf("%s/cases/southern-california_cases.csv", out.dir))
+  write.csv(us.state.cases, sprintf("%s/cases/us-state_cases.csv", out.dir))
+  write.csv(us.county.cases, sprintf("%s/cases/us-county_cases.csv", out.dir))
+  write.csv(global.cases, sprintf("%s/cases/global_cases.csv", out.dir))
+  write.csv(us.state.deaths, sprintf("%s/deaths/us-state_deaths.csv", out.dir))
+  write.csv(us.county.deaths, sprintf("%s/deaths/us-county_deaths.csv", out.dir))
+  write.csv(global.deaths, sprintf("%s/deaths/global_deaths.csv", out.dir))
+  
+}
+
+if (sys.nframe() == 0L) {
+  main()
+}
